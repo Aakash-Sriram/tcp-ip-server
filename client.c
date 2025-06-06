2a65810fff969bb2aa7f9666377d29ed53ea97ce
Aakash Sriram <194975502+Aakash-Sriram@users.noreply.github.com>
2025-06-06 11:43:15 +0530

    done for now

--- client.c
+++ client.c
@@ -35,15 +35,22 @@ int main(){
   int no;
   printf("Enter no of messages to send:");
   scanf("%d",&no);
+  getchar();
   printf("\n");
   while(no>0){
-    printf("Enter text: "); 
+    printf("Enter text(or enter exit to quit): "); 
     char message[1024];
     scanf("%1023s",message);
+    getchar();
+    if(strncmp(message,"exit",4)==0){
+      printf("\nBYE\n");
+      break;
+    }
     int sent = send(s,message,strlen(message),0);
     if(sent<0){
       printf("didnt work");
       close(s);
+      break;
     }
     no--;
   }
